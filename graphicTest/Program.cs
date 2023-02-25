75fa629b5ea22b47e2eaf6bc787efb23f3d04c32
LiberRuth <124418235+LiberRuth@users.noreply.github.com>
2023-02-25 11:58:37 +0900

    초기 커밋.

---
+++ graphicTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace graphicTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(win wins = new win()) 
+            {
+                wins.Run();            
+            }
+        }
+    }
+}
---
+++ graphicTest/win.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+
+namespace graphicTest
+{
+    internal class win : GameWindow
+    {
+        private int vertexBufferHandle;
+
+        public win()
+            : base(GameWindowSettings.Default, NativeWindowSettings.Default) 
+        {
+            this.CenterWindow(new Vector2i(1280, 768));
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            GL.Viewport(0, 0, e.Width, e.Height);
+            base.OnResize(e);
+        }
+
+
+        protected override void OnLoad()
+        {
+            GL.ClearColor(new Color4(0.3f, 0.4f, 0.5f, 1f));
+            float[] vertices = new float[] 
+            { 
+                0.0f, 0.5f, 0f,
+                0.5f, -0.5f, 0f
+                -0.5f, -0.5f, 0f,   
+            };
+
+            this.vertexBufferHandle = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, this.vertexBufferHandle);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+
+            base.OnLoad();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+    }
+}
